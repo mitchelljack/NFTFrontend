409e131dada1e4ceb35802f6470043e6387db5a1
mitchelljack <mitchelljack979@gmail.com>
2022-05-01 03:12:26 +0500

    Completed the task of NFT Frontend Design

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: HomeView,
+    },
+    {
+      path: "/modal",
+      name: "modal",
+      component: import("../views/Modal.vue"),
+    },
+  ],
+});
+
+export default router;
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class='home'>
+    <header><p>Portfolio</p>
+    <div><p>ETH</p><div class="circle-block"><i class="fa-solid fa-left-long"></i><i class="fa-solid fa-right-long"></i></div></div></header>
+    <SparkLine />
+    <Collection />
+  </div>
+</template>
+<script>
+import SparkLine from "./SparkLine.vue";
+import Collection from "./Collection.vue";
+export default {
+  components: { SparkLine, Collection },
+  data() {
+    return {};
+  },
+};
+</script>
